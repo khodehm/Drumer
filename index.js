7a2782465bb718b556dcf845ab8d4387ba7b4baa
alireza hamidi <127328597+khodehm@users.noreply.github.com>
2023-11-12 12:59:43 +0330

    Complite Animation

--- index.js
+++ index.js
@@ -24,55 +24,67 @@ function power(num1,num2) {
 function calculator(num1 ,num2 ,operator) {
     return operator(num1,num2);
 }
-/*function SrcSwap(fileLocation) {
+function SrcSwap(fileLocation) {
     this.fileLocation=fileLocation;
-    this.play=function (){
-        fileLocation.play();
-    }
-}*/
+    let audio=new Audio(fileLocation);
+    audio.play();
+}
 function wiplash (key) {
-
     switch (key) {
         case 'w':
-            var audio=new Audio('./sounds/Hi-hat.mp3');
-            audio.play();
+            let audio=new SrcSwap('./sounds/Hi-hat.mp3');
             break;
         case 'a':
-            audio=new Audio('./sounds/tom-2.mp3');
-            audio.play();
+            let tom2=new SrcSwap('./sounds/tom-2.mp3');
             break;
         case 's':
-            audio=new Audio('./sounds/closed-high-hat-.mp3');
-            audio.play();
+            let closedhighhat=new SrcSwap('./sounds/closed-high-hat-.mp3');
             break;
         case 'd' :
-            audio=new Audio('./sounds/Hi-hat.mp3');
-            audio.play();
+            let hihat=new SrcSwap('./sounds/Hi-hat.mp3');
             break;
         case 'j':
-            audio=new Audio('./sounds/snare.mp3');
-            audio.play();
+            let snare=new SrcSwap('./sounds/snare.mp3');
             break;
         case 'k':
-            audio=new Audio('./sounds/kick-base.mp3');
-            audio.play();
+            let kick=new SrcSwap('./sounds/kick-base.mp3');
             break;
         case 'l':
-            audio=new Audio('./sounds/clean-snare.mp3');
-            audio.play();
+            let cleanSnare=new SrcSwap('./sounds/clean-snare.mp3');
             break;
         default:
             console.log('ended');
 
     }
 }
+function buttonAnimation(currentKey) {
+    document.querySelector('.'+currentKey).classList.add('pressed');
+    setTimeout(function (){
+        document.querySelector('.'+currentKey).classList.remove('pressed');
+    },50);
+}
+/*function newEvent(eventtype,callBack) {
+    anotherevent ={
+        typeOfEvent:'keypress',
+        kybordEvent:'p',
+        duration:2
+    }
+    if (anotherevent.typeOfEvent===eventtype)
+        callBack(eventtype);
+}
+document.addEventListener('keypress', function (event) {
+    newEvent('p',event);
+    console.log(event);
+});*/
 const numberOfButtons = document.querySelectorAll('.drum').length;
 for (let i = 0; i < numberOfButtons; i++) {
-    document.querySelectorAll('.drum')[i].addEventListener('click', function (){
-        let buttonText= this.innerHTML;
-        wiplash(buttonText);
-    });
-}
+document.querySelectorAll('.drum')[i].addEventListener('click', function (){
+    let buttonText= this.innerHTML;
+    wiplash(buttonText);
+    buttonAnimation(buttonText);
+});
+    }
 document.addEventListener('keypress', function (event){
     wiplash(event.key);
+    buttonAnimation(event.key);
 });
